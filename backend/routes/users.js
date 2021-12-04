da3bff1795c44f2b4907bed17a23552cb7c9aea3
akanaka <kiburst0211@gmail.com>
2021-12-04 14:08:05 +0700

    dasdsa

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,18 +1,19 @@
-const Router = require('express').Router();
+const Router = require("express").Router();
 
-let User = require('../model/user.model');
+let User = require("../model/user.model");
 
-
-Router.param('/', (req, res) => {
-
-    User.find({ username: req.username, password: req.password }).then(user => res.json(user)).catch(err => res.status(400).json(err));
+Router.route("/").get( (req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
-Router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const newUser = new User({ username, password });
-    newUser.save()
-        .then(() => res.json({ addStatus: 'success' })).catch(err => res.status(400).json(err));
-
-})
+Router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const newUser = new User({ username, password });
+  newUser
+    .save()
+    .then(() => res.json({ addStatus: "success" }))
+    .catch((err) => res.status(400).json(err));
+});
 module.exports = Router;
